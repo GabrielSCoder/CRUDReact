3484e3af2adf33b9c1259dd93209710fd0659244
GabrielSCoder <gabrielsenasaraiva7@gmail.com>
2023-09-18 10:58:49 -0300

    18-09-23

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -1,10 +1,8 @@
 import { Fragment } from "react"
 export default function Head() {
  return (
- <Fragment>
     <header class="text-center py-2 bg-secondary w-100">
         <h3 class="text-white">Contatos</h3>
     </header>
- </Fragment>
  )
 }
